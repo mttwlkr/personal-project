2ac442a397724555e97cf3ad78f855b0684d7afb
Matt Walker <mttwlkr@gmail.com>
2018-04-06 09:20:14 -0600

    Update create store method

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { createStore } from 'redux';
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+)
 
 
 ReactDOM.render(
