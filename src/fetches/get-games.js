4fad46dab974e2581b4c8cd99f4849405b066c0f
Matt Walker <mttwlkr@gmail.com>
2018-04-09 19:08:21 -0600

    Add page navigation buttons and more game card styling

--- src/fetches/get-games.js
+++ src/fetches/get-games.js
@@ -1,7 +1,7 @@
-export const getGames = async () => {
+export const getGames = async (pageNumber) => {
   try {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";    
-    const url = "slvsh.com/posts.json?page=1&is_active=true";
+    const url = `slvsh.com/posts.json?page=${pageNumber}&is_active=true`;
     const response = await fetch(proxyurl + url)
     const games = await response.json()
     return games
